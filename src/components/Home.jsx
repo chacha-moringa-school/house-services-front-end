745ffee99f9205038acbe0c062ecad4c92f22dbb
iamchacha021 <iamchacha021@gmail.com>
2023-03-01 11:45:18 +0300

    import material UI

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,14 @@
+import Typography from '@mui/material/Typography';
 const Home = () => {
     return ( 
         <div>
-            <h1>HOUSE SERVICES 🏠</h1>
+            <Typography variant="h1" gutterBottom>
+            HOUSE SERVICES 🏠
+            </Typography>
+            
+            <div>
+                <img src="https://images.unsplash.com/photo-1565660761890-0ecd9606690f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80" alt="" />
+            </div>
         </div>
      );
 }
