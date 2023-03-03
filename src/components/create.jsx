96cc0b9e852e042a0558f35a891674e7b810872f
lizdmukami <lizdmukami@gmail.com>
2023-03-03 04:01:11 +0300

    Added styling to the add-service component

--- src/components/create.jsx
+++ src/components/create.jsx
@@ -1,69 +1,124 @@
-import { useEffect } from "react";
-import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Create = () => {
-    const [name, setName] = useEffect("")
-    const [description, setDescription] = useEffect("")
-    const [price, setPrice] = useEffect(0)
-    const [image_url, setImageUrl] = useEffect("")
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState(0);
+  const [image_url, setImageUrl] = useState("");
 
-    let navigate = useNavigate()
+  let navigate = useNavigate();
 
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+    const service = { name, description, price, image_url };
 
-    const handleSubmit = (ev)=>{
-        ev.preventDefault()
-        const service = {name, description, price, image_url}
-        
-        fetch('http://localhost:4000/services',{
-            method: 'POST',
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(service)
-        })
-        .then((res)=>{
-            navigate("/")
-            return res.json()})
-        .then((data)=>{console.log(data)})
-    }
-    
+    fetch("http://localhost:4000/services", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(service),
+    })
+      .then((res) => {
+        navigate("/services");
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  };
 
-    return ( 
-        <div id= "create">
-            <h2>Add New Service</h2>
-            <form action="POST" onSubmit={handleSubmit}>
-                <label>Service name</label>
-                <input 
-                    type="text"
-                    required
-                    value={name}
-                    onChange={(ev)=> setName(ev.target.value)}
-                 />
-
-                 <label >Service description</label>
-                 <textarea 
+  return (
+    <div>
+      <div id="add-service">
+        <div className="container">
+          <div className="col-md-8">
+            <div className="row">
+              <div className="section-title">
+                <h2>Add New Service</h2>
+              </div>
+              <form
+                action="POST"
+                name="sentMessage"
+                validate
+                onSubmit={handleSubmit}
+              >
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        className="form-control"
+                        placeholder="Enter Service name"
+                        required
+                        value={name}
+                        onChange={(ev) => setName(ev.target.value)}
+                      />
+                      <p className="help-block text-danger"></p>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <input
+                        type="number"
+                        id="number"
+                        name="pri"
+                        className="form-control"
+                        placeholder="Enter Price"
+                        required
+                        value={price}
+                        onChange={(ev) => setPrice(ev.target.value)}
+                      />
+                      <p className="help-block text-danger"></p>
+                    </div>
+                  </div>
+                </div>
+                <div className="form-group">
+                  <div className="form-group">
+                    <input
+                      type="url"
+                      id="email"
+                      name="email"
+                      className="form-control"
+                      placeholder="Enter Image-url"
+                      required
+                      value={image_url}
+                      onChange={(ev) => setImageUrl(ev.target.value)}
+                    />
+                    <p className="help-block text-danger"></p>
+                  </div>
+                </div>
+                <div className="form-group">
+                  <textarea
+                    name="message"
+                    id="message"
+                    className="form-control"
+                    rows="4"
+                    placeholder="Description"
                     required
                     value={description}
-                    onChange={(ev)=> setDescription(ev.target.value)}
-                 ></textarea>
-                 
-                <label>Service price</label>
-                <input 
-                    type="number"
-                    required
-                    value={price}
-                    onChange={(ev)=> setPrice(ev.target.value)}
-                 />
-
-                <label>Image url</label>
-                <input 
-                    type="url"
-                    required
-                    value={image_url}
-                    onChange={(ev)=> setImageUrl(ev.target.value)}
-                 />
-                 <button>Submit</button>
-            </form>
+                    onChange={(ev) => setDescription(ev.target.value)}
+                  ></textarea>
+                  <p className="help-block text-danger"></p>
+                </div>
+                <div id="success"></div>
+                <button type="submit" className="btn btn-custom btn-lg">
+                  Submit
+                </button>
+              </form>
+            </div>
+          </div>
         </div>
-     );
-}
- 
+      </div>
+      <div id="footer">
+        <div className="container text-center">
+          <p>&copy; 2023 Nice Touch. All Rights Reserved!</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Create;
+    
