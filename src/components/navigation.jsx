2f63d74838f3ce49664be59f210acb1b19fd1739
lizdmukami <lizdmukami@gmail.com>
2023-03-03 04:57:07 +0300

    updated delete request

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -47,7 +47,7 @@ export const Navigation = (props) => {
               </a>
             </li>
             <li>
-              <a href="#contact" className="page-scroll">
+              <a href="/#contact" className="page-scroll">
                 Contact
               </a>
             </li>
